7e45b11ca3a0137236d72a020275b4550b998d2b
Tianyao Gu <gty929@sjtu.edu.cn>
2024-08-14 23:47:55 -0400

    add get benchmark

--- benches/db_bench.rs
+++ benches/db_bench.rs
@@ -51,7 +51,7 @@ fn benchmark_db_insert_varied_val(c: &mut Criterion) {
         let value = vec![0; value_size];
         db.insert(key.to_vec(), value);
     }
-    c.bench_function("db_insert_large", |b| {
+    c.bench_function("db_insert_varied", |b| {
         b.iter(|| {
             let key = rand::random::<[u8; KEY_SIZE]>();
             let value_size = rand::random::<usize>() % 512;
@@ -61,10 +61,67 @@ fn benchmark_db_insert_varied_val(c: &mut Criterion) {
     });
 }
 
+fn benchmark_db_get_100k(c: &mut Criterion) {
+    let db = ObliviousDB::new();
+    const KEY_SIZE: usize = 32;
+    const VALUE_SIZE: usize = 512;
+    const DB_SIZE: usize = 100000;
+    for _ in 0..DB_SIZE {
+        let key = rand::random::<[u8; KEY_SIZE]>();
+        let value = vec![0; VALUE_SIZE];
+        db.insert(key.to_vec(), value);
+    }
+    c.bench_function("db_get_100k", |b| {
+        b.iter(|| {
+            let key = rand::random::<[u8; KEY_SIZE]>();
+            db.get(&key);
+        })
+    });
+}
+
+fn benchmark_db_get_1m(c: &mut Criterion) {
+    let db = ObliviousDB::new();
+    const KEY_SIZE: usize = 32;
+    const VALUE_SIZE: usize = 512;
+    const DB_SIZE: usize = 1000000;
+    for _ in 0..DB_SIZE {
+        let key = rand::random::<[u8; KEY_SIZE]>();
+        let value = vec![0; VALUE_SIZE];
+        db.insert(key.to_vec(), value);
+    }
+    c.bench_function("db_get_1m", |b| {
+        b.iter(|| {
+            let key = rand::random::<[u8; KEY_SIZE]>();
+            db.get(&key);
+        })
+    });
+}
+
+fn benchmark_db_get_10m_small_val(c: &mut Criterion) {
+    let db = ObliviousDB::new();
+    const KEY_SIZE: usize = 32;
+    const VALUE_SIZE: usize = 32;
+    const DB_SIZE: usize = 10000000;
+    for _ in 0..DB_SIZE {
+        let key = rand::random::<[u8; KEY_SIZE]>();
+        let value = vec![0; VALUE_SIZE];
+        db.insert(key.to_vec(), value);
+    }
+    c.bench_function("db_get_10m", |b| {
+        b.iter(|| {
+            let key = rand::random::<[u8; KEY_SIZE]>();
+            db.get(&key);
+        })
+    });
+}
+
 criterion_group!(
     benches,
     benchmark_db_insert_small_kv,
     benchmark_db_insert_large_kv,
-    benchmark_db_insert_varied_val
+    benchmark_db_insert_varied_val,
+    benchmark_db_get_100k,
+    benchmark_db_get_1m,
+    benchmark_db_get_10m_small_val
 );
 criterion_main!(benches);
